0a824763be4e0fb2b057b719b3f8b6b3b64c5424
alexcengler <alexcengler@gmail.com>
2016-07-15 00:16:40 -0400

    Aesthetics and basic scatterplot, scales, axes

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
-# Final Project Template
 
-## Getting started
 
-When setting up the project for the first time, you'll need to:
 
-* Download the template as a ZIP archive, unarchive it, and save it somewhere on your computer. You'll probably want to rename the project directory from "final-project-template-master" to something that's appropriate for your project.
-* `cd` into the project directory.
-* Run `git init` to make it a git repository.
-* Let's make our first commit! `git add -A` and then `git commit -m "Initial commit"`.
-* On GitHub, let's create a new remote repository by going to "New repository" in the "+" menu in the upper right hand corner of GitHub.
-* Give your repository an appropriate name, probably similar to the directory name you used above, and a description.
-* The defaults are fine for the other fields.
-* We now run the commands in the "...or push an existing repository from the command line" example shown on the resulting page.
+// % TANF Enrollment of Eligible 
+// Avg Dollars per person
 
-## Running a local server
+// One over time, then both in directed scatterplot, then breakout into state data like NYT education graphs.
 
-If you haven't yet installed [reload](https://www.npmjs.com/package/reload) globally, run `npm install -g reload'.
+// http://www.nytimes.com/interactive/2016/04/29/upshot/money-race-and-success-how-your-school-district-compares.html
 
-After that, any time you want to work on your project, run `reload -b` from the project directory, perhaps in a second tab so you can continue to use git. (If it doesn't work, it could be a port conflict, so you might try specifying a different port like this: `reload -b -p 4567`.)
+// TANF can't respond to increased caseloads
+// States have incentives to reduce caseloads
+
+// data.json is TANF-to-Povery Ratios by State from here: http://www.cbpp.org/research/family-income-support/tanf-continues-to-weaken-as-a-safety-net
+
--- script.js
+++ script.js
@@ -1,186 +1,84 @@
-var app;
 
-// Declaring our constants
-var START_YEAR = 1950;
-var END_YEAR = 2015;
-var MAX_RADIUS = 50;
-var TRANSITION_DURATION = 750;
 
-// d3.queue() enables us to load multiple data files. Following the example below, we make
-// additional .defer() calls with additional data files, and they are returned as results[1],
-// results[2], etc., once they have all finished downloading.
+// fam-w-children-tanf-ratio.json
+// CBPP Table 2: National Single-Year TANF-to-Poverty Ratios
+// http://www.cbpp.org/research/family-income-support/tanf-continues-to-weaken-as-a-safety-net
+
+
 d3.queue()
-  .defer(d3.json, 'data/data.json')
+  .defer(d3.json, 'data/fam-w-children-tanf-ratio.json')
   .awaitAll(function (error, results) {
     if (error) { throw error; }
-    app.initialize(results[0]);
+    directedScatterPlot(results[0]);
   });
 
-app = {
-  data: [],
-  components: [],
 
-  options: {
-    year: START_YEAR
-  },
+var margin = {
+	left: 100,
+	right: 50,
+	top: 100,
+	bottom: 50
+};
 
-  initialize: function (data) {
-    app.data = data;
 
-    // Here we create each of the components on our page, storing them in an array
-    app.components = [
-      new Chart('#chart')
-    ];
-
-    // Add event listeners and the like here
-
-    // app.resize() will be called anytime the page size is changed
-    d3.select('window').on('resize', app.resize);
-
-    // For demo purposes, let's tick the year every 750ms
-    function incrementYear() {
-      app.options.year += 1;
-      if (app.options.year > END_YEAR) {
-        app.options.year = START_YEAR;
-      }
-
-      app.update();
-    }
-
-    d3.interval(incrementYear, TRANSITION_DURATION);
-  },
-
-  resize: function () {
-    app.components.forEach(function (c) { if (c.resize) { c.resize(); }});
-  },
-
-  update: function () {
-    app.components.forEach(function (c) { if (c.update) { c.update(); }});
-  }
-}
-
-function Chart(selector) {
-  var chart = this;
-
-  // SVG and MARGINS
-
-  var margin = {
-    top: 15, right: 15, bottom: 40, left: 45
-  };
-
-  chart.width = 600 - margin.left - margin.right;
-  chart.height = 400 - margin.top - margin.bottom;
-
-  chart.svg = d3.select(selector)
-    .append('svg')
-    .attr('width', chart.width + margin.left + margin.right)
-    .attr('height', chart.height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-  // SCALES
-
-  chart.x = d3.scaleLinear()
-    .domain([0, d3.max(app.data, function (d) { return d.total_fertility; })])
-    .range([0, chart.width])
-    .nice();
-
-  chart.y = d3.scaleLinear()
-    .domain([0, d3.max(app.data, function (d) { return d.life_expectancy; })])
-    .range([chart.height, 0])
-    .nice();
-
-  chart.r = d3.scaleSqrt()
-    .domain([0, d3.max(app.data, function (d) { return d.population; })])
-    .range([0, MAX_RADIUS]);
-
-  chart.color = d3.scaleOrdinal(d3.schemeCategory10);
-
-  // AXES
-
-  var xAxis = d3.axisBottom()
-    .scale(chart.x);
-
-  var yAxis = d3.axisLeft()
-    .scale(chart.y);
-
-  chart.svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + chart.height + ')')
-    .call(xAxis)
-    .append('text')
-    .attr('y', 30)
-    .attr('x', chart.width)
-    .style('text-anchor', 'end')
-    .style('fill', '#000')
-    .style('font-weight', 'bold')
-    .text('Fertility (births per woman)');
-
-  chart.svg.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis)
-    .append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('dy', '.71em')
-    .attr('y', -35)
-    .attr('x', 0)
-    .style('text-anchor', 'end')
-    .style('fill', '#000')
-    .style('font-weight', 'bold')
-    .text('Life expectancy (years)');
-
-  // YEAR LABEL
-
-  chart.svg.append('text')
-    .attr('class', 'year')
-    .attr('x', chart.width / 2)
-    .attr('y', chart.height / 2)
-    .attr('dy', '.35em')
-    .style('text-anchor', 'middle')
-    .style('font-size', '230px')
-    .style('font-weight', 'bold')
-    .style('opacity', 0.2)
-    .text(app.options.year);
-
-  chart.update();
-}
-
-Chart.prototype = {
-  update: function () {
+function directedScatterPlot(data) {
+    
     var chart = this;
 
-    // TRANSFORM DATA
+    chart.width = 600 - margin.left - margin.right;
+    chart.height = 600 - margin.top - margin.bottom;
 
-    var txData = app.data.filter(function (d) { return d.year === app.options.year; });
+    chart.svg = d3.select("#chart")
+    	.append("svg")
+    	.attr("width", chart.width + margin.left + margin.right)
+    	.attr("height", chart.height + margin.top + margin.bottom)
+    	.append("g")
+    	.attr("transform", function(){ return "translate(" + margin.left + "," + margin.top + ")" });
 
-    // UPDATE CHART ELEMENTS
+    chart.xScale = d3.scaleLinear()
+      	.domain([0, d3.max(data, function (d) { return d.fam_child_pov; })])
+    	.range([0, width]);
 
-    var t = d3.transition().duration(TRANSITION_DURATION);
+    chart.yScale = d3.scaleLinear()
+      	.domain([d3.max(data, function (d) { return d.fam_child_pov; }), 0])
+    	.range([0, height]);
 
-    var yearText = d3.selectAll('.year')
-      .transition().delay(TRANSITION_DURATION / 2)
-      .text(app.options.year);
+    var xAxis = d3.axisBottom(chart.xScale).ticks(10);
+	var yAxis = d3.axisLeft(chart.yScale).ticks(10);
 
-    var countries = chart.svg.selectAll('.country')
-      .data(txData, function (d) { return d.country; });
+    chart.svg.append("g")
+    	.attr("transform", function(){ return "translate(0," + chart.height + ")" })
+    	.attr("class", "axis")
+    	.call(xAxis);
 
-    countries.enter().append('circle')
-      .attr('class', 'country')
-      .style('fill', function (d) { return chart.color(d.continent); })
-      .style('opacity', 0.75)
-      .attr('r', 0)
-      .attr('cx', chart.width / 2)
-      .attr('cy', chart.height / 2)
-      .merge(countries)
-      .sort(function (a, b) { return b.population - a.population; })
-      .transition(t)
-      .attr('r', function (d) { return chart.r(d.population); })
-      .attr('cx', function (d) { return chart.x(d.total_fertility); })
-      .attr('cy', function (d) { return chart.y(d.life_expectancy); });
+    chart.svg.append("g")
+    	.attr("class", "axis")
+    	.call(yAxis);
 
-    countries.exit()
-      .transition(t)
-      .attr('r', 0)
-      .remove();
-  }
-}
+    chart.svg
+    	.selectAll(".circ")
+    	.data(data, function(d){ return d.year }).enter()
+    	.append("circle")
+    	.attr("class", "circ")
+    	.attr("cx", function(d){ return chart.xScale(d.fam_child_pov) })
+    	.attr("cy", function(d){ return chart.yScale(d.tanf_fam) })
+        .style('opacity', 0.75)
+    	.attr("r", 5)
+    	.attr("fill", "black");
+
+    chart.svg
+    	.selectAll("circle")
+
+
+
+};	
+
+
+
+
+
+
+
+
+
+
